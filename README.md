2b136615fb4daf4c598c27dec2950dc14d52140b
zondayang <u226699@gmail.com>
2021-07-18 09:35:30 +0800

    Add example: file encode

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Example
 
+### Python Dictionary
+
 ```python
 from datetime import datetime
 
@@ -34,16 +36,56 @@ b'\x8a\xa3str\xa11\xa4byte\xc4\x04test\xa5float\xca?\x8c\xcc\xcd\xa3int\xff\xa4N
 """
 ```
 
-### ExtStruct
+#### ExtStruct
 
 ```python
-# example: id = 1, data = b"test
+# example: type = 1, data = b"test
 # define
 ExtStruct(1, b"test")
 
 # 2021/07/17 22:30:45.123456
 # Must be datetime
 TimestampStruct(datetime.strptime("2021/07/17 22:30:45.123456", "%Y/%m/%d %H:%M:%S.%f"))
+```
+
+### File
+
+example.json  
+
+```json
+{
+    "str": "1",
+    "float": 1.1,
+    "int": -1,
+    "None": null,
+    "bool": false,
+    "array": [true, true],
+    "dict": {
+        "test": "test",
+        "test2": 2
+    }
+}
+```
+
+```python
+original_data = {}
+with open("./example.json", "r") as f:
+    content = f.read()
+    original_data = json.loads(content)
+
+encoded_data = encoder(original_data)
+
+print(f"Length: {len(encoded_data)}")
+print(encoded_data)
+print(" ".join(convert_bytes_to_hex_list(encoded_data)))
+
+with open("./example.msgpack", "wb") as f:
+    f.write(encoded_data)
+"""
+Length: 67
+b'\x87\xa3str\xa11\xa5float\xca?\x8c\xcc\xcd\xa3int\xff\xa4None\xc0\xa4bool\xc2\xa5array\x92\xc3\xc3\xa4dict\x82\xa4test\xa4test\xa5test2\x02'
+87 a3 73 74 72 a1 31 a5 66 6c 6f 61 74 ca 3f 8c cc cd a3 69 6e 74 ff a4 4e 6f 6e 65 c0 a4 62 6f 6f 6c c2 a5 61 72 72 61 79 92 c3 c3 a4 64 69 63 74 82 a4 74 65 73 74 a4 74 65 73 74 a5 74 65 73 74 32 02
+"""
 ```
 
 ## Test
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from config.settings import setup_log
 setup_log("msgpack")
 
+import json
 from typing import List
 from datetime import datetime
 
@@ -15,25 +16,45 @@ def convert_bytes_to_hex_list(data: bytes) -> List:
         hex_list.append(hex_data[i:i+2])
     return hex_list
 
+def dict_example():
+    original_data = {
+        "str": "1",
+        "byte": b"test",
+        "float": 1.1,
+        "int": -1,
+        "None": None,
+        "bool": False,
+        "array": [True, True],
+        "dict": {
+            "test": "test",
+            "test2": 2
+        },
+        "ext": ExtStruct(1, b"test"),
+        "timestamp": TimestampStruct(datetime.strptime("2021/07/17 22:30:45.123456", "%Y/%m/%d %H:%M:%S.%f"))
+    }
 
-original_data = {
-    "str": "1",
-    "byte": b"test",
-    "float": 1.1,
-    "int": -1,
-    "None": None,
-    "bool": False,
-    "array": [True, True],
-    "dict": {
-        "test": "test",
-        "test2": 2
-    },
-    "ext": ExtStruct(1, b"test"),
-    "timestamp": TimestampStruct(datetime.strptime("2021/07/17 22:30:45.123456", "%Y/%m/%d %H:%M:%S.%f"))
-}
+    encoded_data = encoder(original_data)
 
-encoded_data = encoder(original_data)
+    print(f"Length: {len(encoded_data)}")
+    print(encoded_data)
+    print(" ".join(convert_bytes_to_hex_list(encoded_data)))
 
-print(f"Length: {len(encoded_data)}")
-print(encoded_data)
-print(" ".join(convert_bytes_to_hex_list(encoded_data)))
+def file_example(file_path: str, encoded_file_path: str):
+    original_data = {}
+    with open(file_path, "r") as f:
+        content = f.read()
+        original_data = json.loads(content)
+
+    encoded_data = encoder(original_data)
+
+    print(f"Length: {len(encoded_data)}")
+    print(encoded_data)
+    print(" ".join(convert_bytes_to_hex_list(encoded_data)))
+
+    with open(encoded_file_path, "wb") as f:
+        f.write(encoded_data)
+
+
+if __name__ == "__main__":
+    dict_example()
+    file_example("./example.json", "./example.msgpack")
